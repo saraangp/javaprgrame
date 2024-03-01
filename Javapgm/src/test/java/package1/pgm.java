054ac84a8ff5e6db6f94bbe70daed40c44da55f3
saraangp <sarangpawar146@gmail.com>
2024-03-01 19:39:48 +0530

    This is first commit

---
+++ Javapgm/src/test/java/package1/pgm.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class pgm {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello Java");
+		System.out.println("This is a Java Programme");
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
